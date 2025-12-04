b91658e80c9fb17acfa5632d7a86468bdccd8836
swapnil404 <swapnil.christian.developer@gmail.com>
2025-12-04 22:45:46 +0530

    solution of https://leetcode.com/problems/longest-substring-without-repeating-characters/

---
+++ functions/ongest-substring-without-repeating-characters-leetcode.js
@@ -0,0 +1,32 @@
+/** * @param {string} s * @return {number} */
+var lengthOfLongestSubstring = function (s) {
+  let number = 0;
+  if (s.length == 1) {
+    number = 1;
+  } else {
+    function hasRepeats(str) {
+      const set = new Set();
+      for (let char of str) {
+        if (set.has(char)) {
+          return true;
+        }
+        set.add(char);
+      }
+      return false;
+    }
+    const substrings = [];
+    for (let i = 0; i < s.length; i++) {
+      for (let j = i; j < s.length; j++) {
+        substrings.push(s.slice(i, j + 1));
+      }
+    }
+    for (let k = 0; k < substrings.length; k++) {
+      if (substrings[k].length > number) {
+        if (!hasRepeats(substrings[k])) {
+          number = substrings[k].length;
+        }
+      }
+    }
+  }
+  return number;
+};
